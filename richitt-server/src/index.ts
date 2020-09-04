62e64abd29a10b95e16b2f7e0ba810a7be34d20b
Richard Hoffmann <rhoffmann@fastmail.com>
2020-09-04 14:53:18 +0200

    add redis container and setup session for express

--- richitt-server/src/index.ts
+++ richitt-server/src/index.ts
@@ -8,6 +8,15 @@ import express from "express";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { REDIS_SECRET, SESSION_COOKIE_NAME, __prod__ } from "./constants";
+import { MyContext } from "types";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
 const port = process.env.PORT || 4000;
 
 const main = async () => {
@@ -16,13 +25,35 @@ const main = async () => {
 
   const app = express();
 
+  app.use(
+    session({
+      name: SESSION_COOKIE_NAME,
+      store: new RedisStore({
+        client: redisClient,
+        // disableTTL: true,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // only on https (prod)
+      },
+      saveUninitialized: false,
+      secret: REDIS_SECRET,
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({
+    context: ({ req, res }): MyContext => ({
       em: orm.em,
+      req,
+      res,
     }),
   });
 
--- richitt-server/src/resolvers/user.ts
+++ richitt-server/src/resolvers/user.ts
@@ -103,7 +103,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, {
       username: options.username,
@@ -124,6 +124,8 @@ export class UserResolver {
         errors: [{ field: "password", message: "password is incorrect" }],
       };
     }
+
+    req.session!.userId = user.id;
 
     return { user };
   }
